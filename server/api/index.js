37a73f9eb5694bae0904b8803f4c0c5244181c83
Anna Morozova <anna.mik.morozova@gmail.com>
2020-07-01 13:07:00 -0400

    Feat: add a fake route in api

--- server/api/index.js
+++ server/api/index.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
 
 //api routes go here
+router.get("/", (req, res, next) => {
+    res.json("This is working lol just jk")
+})
 
 router.use((req, res, next) => {
     const error = new Error('Not Found');
