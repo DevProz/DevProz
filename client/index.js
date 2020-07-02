fabe2ffcb408eb44fd3d9c8a0c043a712e40d515
Anna Morozova <anna.mik.morozova@gmail.com>
2020-07-02 17:58:37 -0400

    Feat: connect server and client

--- client/index.js
+++ client/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {App} from "./components/App";
+import App from "./components/App";
 import {Provider} from "react-redux";
 
 ReactDOM.render(
-    <App />,
+    <App /> ,
   document.getElementById('app') // make sure this is the same as the id of the div in your index.html
 );
