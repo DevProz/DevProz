5447cf15f55a1fb911e070db3d8930da1164d448
Samantha Hering <samanthahering@yahoo.com>
2020-07-06 17:48:18 -0400

    chore: created a newGame component

--- client/store.js
+++ client/store.js
@@ -3,36 +3,58 @@ import thunkMiddleware from 'redux-thunk';
 import axios from 'axios'
 
 
+
 //action types
-const GET_PLAYERS = 'GET_PLAYERS'
+//const GET_PLAYERS = 'GET_PLAYERS';
+const CREATE_NEW_GAME = 'CREATE_NEW_GAME';
 
 //action creator
-export const getPlayers = (players) => {
+// export const getPlayers = (players) => {
+//     return {
+//         type: GET_PLAYERS,
+//         players
+//     }
+// }
+
+export const createNewGame = (game) => {
     return {
-        type: GET_PLAYERS,
-        players
+        type: CREATE_NEW_GAME,
+        game
     }
 }
 
 
 
 //thunk
-export const fetchPlayers = () => async (dispatch) => {
-    const { data } = await axios.get('/api/players')
-    dispatch(getPlayers(data))
+// export const fetchPlayers = () => async (dispatch) => {
+//     const { data } = await axios.get('/api/players')
+//     console.log(data)
+//     dispatch(getPlayers(data))
+// }
+
+export const fetchNewGame = (game) => async (dispatch) => {
+    try {
+        const { data } = await axios.post('/api/newGame', game)
+        dispatch(createNewGame(data))
+        
+    } catch (error) {
+        console.log('this is the error!', error)
+        
+    }
 }
 
 
 const initialState = {
-    players: []
+    game: {}
+
 }
 
 
 //reducer
 const reducer = (state = initialState, action) => {
     switch(action.type) {
-        case GET_PLAYERS:
-            return {...state, players: action.players}
+        case CREATE_NEW_GAME:
+            return {...state, game: action.game}
         default:
             return state
     }
