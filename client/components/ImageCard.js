66e7482360fa7ae6445844fd2058b31a6f0e0f06
Maria <marybelousova@yahoo.com>
2020-07-09 11:41:51 -0400

    ImageComponent with slice method

--- client/components/ImageCard.js
+++ client/components/ImageCard.js
@@ -4,11 +4,12 @@ import { Card, CardDeck } from 'react-bootstrap'
 
 const ImageCards = (props) => {
     console.log('this is in the image component', props)
-
+        const currentImage=props.imageCards.slice(0, 1)
+        console.log('image after slice', currentImage)
         return (
             <div  className="d-block mx-auto img-fluid w-50" >
               <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src={props.imageCards[0].image}/>
+                <Card.Img variant="top" src={currentImage[0].image}/>
              </Card>   
             </div>
     )
