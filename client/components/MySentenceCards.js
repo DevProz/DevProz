717e1f1f9726efaac348125724c6416695676a37
Samantha Hering <samanthahering@yahoo.com>
2020-07-10 15:10:07 -0400

    update: started updating the submit button

--- client/components/MySentenceCards.js
+++ client/components/MySentenceCards.js
@@ -1,17 +1,34 @@
 import React from "react";
 import { Card } from 'react-bootstrap'
 import { connect } from 'react-redux'
+import socket from '../socket'
 import { me } from '../store'
 
 
 class MySentenceCards extends React.Component {
+    constructor(){
+        super()
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleSubmit(event){
+        event.preventDefault()
+        console.log('clicked!!!!')
+        console.log('this is the event.target', event.target)
+        // socket.emit("start_game", { card: this.props.player._id, code: this.props.game.entranceCode})
+        
+       
+
+
+    }
 
     render () {
+        console.log('this is props', this.props)
         return (
             <div className="cards-row">
                 {this.props.player.sentenceCards.map(card =>
-                    <Card>
-                        <Card.Body style={{width: "10rem"}}>
+                    <Card key={card._id}>
+                        <Card.Body  style={{width: "10rem"}} onClick = {this.handleSubmit}>
                                     {card.sentence}
                         </Card.Body>
                     </Card>
