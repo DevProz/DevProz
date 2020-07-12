953e1eda949a3f347e26361b22516b359725a8c0
Anna Morozova <anna.mik.morozova@gmail.com>
2020-07-12 14:45:08 -0400

    Style: code beauty

--- client/store.js
+++ client/store.js
@@ -2,40 +2,36 @@ import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import socket from './socket'
 import axios from 'axios'
-import {createLogger} from "redux-logger";
+import {createLogger} from 'redux-logger';
 
 //action types
 const GET_PLAYERS = 'GET_PLAYERS';
 const ADD_PLAYER = 'ADD_PLAYER';
 const UPDATE_NEW_GAME = 'UPDATE_NEW_GAME';
 const UPDATE_PLAYER = 'UPDATE_PLAYER ';
-
-const SELECTED_CARD = 'SELECTED_CARD'
-
+const SELECTED_CARD = 'SELECTED_CARD';
 const RECEIVE_MESSAGE = 'RECEIVE_MESSAGE';
-
 
 export const getPlayers = (players) => {
     return {
         type: GET_PLAYERS,
         players
     }
-}
+};
 
 export const updateNewGame = (game) => {
-    console.log("UPDATED")
     return {
         type: UPDATE_NEW_GAME,
         game
     }
-}
+};
 
 export const addedPlayer = (player) => {
     return {
         type: ADD_PLAYER,
         player
     }
-}
+};
 
 export const updatePlayer = player => {
     return {
@@ -43,7 +39,6 @@ export const updatePlayer = player => {
         player
     }
 };
-
 
 const selectedCard = card => {
     return {
@@ -57,51 +52,47 @@ export const receiveMessage = message => {
         type: RECEIVE_MESSAGE,
         message
     }
-}
-
+};
 
 export const me = () => async dispatch => {
     try {
-      const res = await axios.get("/api/newGame/me");
+      const res = await axios.get('/api/newGame/me');
       dispatch(updatePlayer(res.data || defaultUser));
       if (res.data.sentenceCards.length) {
-        const player = {playerId: res.data._id}
-        console.log(res.data)
-        socket.emit("rejoin", player)
+        const player = {playerId: res.data._id};
+        socket.emit('rejoin', player);
       }
-    } catch (err) {
-      console.error(err);
+    } catch(error) {
+      console.error(error);
     }
-  };
-
-
+};
 
 // thunk
 export const fetchPlayers = () => async (dispatch) => {
-    const { data } = await axios.get('/api/players')
-    dispatch(getPlayers(data))
-    
-}
-
-export const fetchNewGame = (player) => async () => {
     try {
-        socket.emit("new_game", {playerId: player._id})
-    } catch (error) {
-        console.log('this is the error!', error)
-        
-    }
-}
-
-
-export const changeName = (playerName) => async (dispatch) => {
-    try {
-    const {data} = await axios.post('/api/players', playerName)
-    dispatch(updatePlayer(data))
+        const { data } = await axios.get('/api/players')
+        dispatch(getPlayers(data))
     } catch(error) {
         console.log(error)
     }
 }
 
+export const fetchNewGame = (player) => async () => {
+    try {
+        socket.emit('new_game', {playerId: player._id})
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+export const changeName = (playerName) => async (dispatch) => {
+    try {
+    const {data} = await axios.post('/api/players', playerName);
+    dispatch(updatePlayer(data));
+    } catch(error) {
+        console.log(error);
+    }
+}
 
 const initialState = {
     game: null,
